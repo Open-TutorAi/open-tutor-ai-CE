8c3f90bd0db21e83ad840dc0aa5e6ba8dd6c7eba
Lahcen ECHCHARIY <echcharyilahcen@gmail.com>
2026-04-30 04:09:12 +0100

    Cleaned the teacher space code (partialy)

--- src/routes/teacher/reports/+page.svelte
+++ src/routes/teacher/reports/+page.svelte
@@ -3,7 +3,4 @@
 	import Reports from '$lib/components/teacher/pages/Reports.svelte';
 </script>
 
-<<<<<<< HEAD
 <Reports />
-=======
->>>>>>> 4f8e763b8ec48fe19c4ef0f70a0b4f2c1b7b4f11
--- src/routes/teacher/settings/+page.svelte
+++ src/routes/teacher/settings/+page.svelte
@@ -3,8 +3,4 @@
     import TeacherSettings from '$lib/components/teacher/pages/Settings.svelte';
 </script>
 
-<<<<<<< HEAD
-<Settings />
-=======
 <TeacherSettings />
->>>>>>> 4f8e763b8ec48fe19c4ef0f70a0b4f2c1b7b4f11
