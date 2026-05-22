a9abd70bc4e96867a4a65651c4a59542a0a16a3e
mohamed <mohamed@gmail.com>
2026-05-22 21:03:53 +0100

    create FileManager component

---
+++ src/lib/components/student/elements/FileManager.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+
+	interface I18n {
+		t: (key: string) => string;
+	}
+	const i18n = getContext<Writable<I18n>>('i18n');
